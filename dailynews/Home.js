eab7e33789642cab3f3a73b20883acd501e69b10
İsmail Berkan Kiraz <63494351+berkankiraz@users.noreply.github.com>
2022-07-05 14:11:07 +0300

    Add files via upload

---
+++ dailynews/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div> 
+       
+
+
+
+
+    <div className="dailynewsfinished">
+         <h1>The most liked posts:</h1>
+    </div>
+    
+
+
+    </div>
+  )
+}
+
+export default Home
